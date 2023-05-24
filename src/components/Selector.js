d1e2655098a08ec7e141e1a13d7f15f52c54748b
powerninja <asdyuy23@gmail.com>
2023-05-25 00:10:49 +0900

    add：選択リストで選択された国の情報を動的に取得する

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import countries from '../countries';
 
 export const Selector = () => {
+  const [country, setCountry] = useState('');
+
+  const getCountryData = () => {
+    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <>
       {/* 選択リスト */}
-      <select>
+      <select onChange={(event) => setCountry(event.target.value)}>
+        <option>Select A Country</option>
         {countries.map((country, index) => (
           <option key={index} value={country.Slug}>
             {country.Country}
           </option>
         ))}
       </select>
+      {country === '' ? <></> : <button onClick={() => getCountryData()}>Get Data</button>}
     </>
   );
 };
