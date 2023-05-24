24be2c0e1ad7cd800c34bae33da17320293093d5
powerninja <asdyuy23@gmail.com>
2023-05-24 23:38:03 +0900

    change:keyとvalueを追加

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,14 +1,14 @@
 import countries from '../countries';
 
 export const Selector = () => {
-  console.log(countries);
-
   return (
     <>
       {/* 選択リスト */}
       <select>
-        {countries.map((country) => (
-          <option>{country.Country}</option>
+        {countries.map((country, index) => (
+          <option key={index} value={country.Slug}>
+            {country.Country}
+          </option>
         ))}
       </select>
     </>
