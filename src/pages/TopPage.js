5842c5883f66c828f3b9be6c6bfb2153d7011e9c
powerninja <asdyuy23@gmail.com>
2023-05-25 22:45:38 +0900

    add：Resultコンポーネント追加

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,11 +1,13 @@
 import { Title } from '../components/Title.js';
 import { Selector } from '../components/Selector.js';
+import { Result } from '../components/Result.js';
 
 export const TopPage = () => {
   return (
     <>
       <Title></Title>
       <Selector></Selector>
+      <Result></Result>
     </>
   );
 };
