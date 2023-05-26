cdf6386ac8acce6f3e198a3ec1581decfe723396
powerninja <asdyuy23@gmail.com>
2023-05-27 00:24:58 +0900

    add：感染者数を格納するuseStateを追加

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import { TopPage } from './pages/TopPage';
 
 export const App = () => {
   const [country, setCountry] = useState('');
+  const [countryData, setCountryData] = useState({
+    date: '',
+    newConfirmed: '',
+    totalConfirmed: '',
+    newRecovered: '',
+    totalRecovered: '',
+  });
 
   const getCountryData = () => {
     fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        console.log(data[data.length - 1]);
       });
   };
   return (
