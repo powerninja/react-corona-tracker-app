833564e39eefccef729950005920a6a7a0c74567
powerninja <asdyuy23@gmail.com>
2023-05-27 10:16:41 +0900

    change：countryDataを分解

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,21 +1,21 @@
 export const Result = ({ countryData }) => {
-  console.log(countryData);
+  const { date, newConfirmed, totalConfirmed, newRecovered, totalRecovered } = countryData;
   return (
     <div className="results-container">
       <p>
-        日付：<span>{countryData.date.slice(0, 10)}</span>
+        日付：<span>{date.slice(0, 10)}</span>
       </p>
       <p>
-        新規感染者数：<span>{countryData.newConfirmed.toLocaleString()}</span>
+        新規感染者数：<span>{newConfirmed.toLocaleString()}</span>
       </p>
       <p>
-        感染者総数：<span>{countryData.totalConfirmed.toLocaleString()}</span>
+        感染者総数：<span>{totalConfirmed.toLocaleString()}</span>
       </p>
       <p>
-        新規回復者：<span>{countryData.newRecovered.toLocaleString()}</span>
+        新規回復者：<span>{newRecovered.toLocaleString()}</span>
       </p>
       <p>
-        回復者総数：<span>{countryData.totalRecovered.toLocaleString()}</span>
+        回復者総数：<span>{totalRecovered.toLocaleString()}</span>
       </p>
     </div>
   );
